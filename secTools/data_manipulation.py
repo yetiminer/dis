b65eedcab60efdc4e47e8380feccad1e6aaae203
Henry Ashton <github@yetiminer.com>
2018-07-04 18:18:58 +0100

    get rid of cases when y is zero

--- secTools/data_manipulation.py
+++ secTools/data_manipulation.py
@@ -3,22 +3,24 @@ import pandas as pd
 
 
 def split_data_X_Y(df,cols):
-    #for the
+	#for the
 
-    Y_feature=cols
+	Y_feature=cols
 
-    tf=df[Y_feature]!=np.nan
+	#tf=df[Y_feature]!=np.nan
+	#tf=~df[Y_feature].isin([0,np.nan]) #apparently there are some zero values floating about
+	df[Y_feature]=df[Y_feature].replace([np.inf,0, -np.inf], np.nan)
+	#df[Y_feature].replace([np.inf,-np.inf,0],np.nan,inplace=True)
+	data=df.dropna(subset=Y_feature, how='any').copy()
+	
 
-    data=df.dropna(subset=Y_feature, how='any').copy()
+	cols=data.columns
+	cols_tf=~cols.isin(Y_feature)
+	cols=cols[cols_tf]
+	X=data[cols].values
+	Y=data[Y_feature].values
 
-
-    cols=data.columns
-    cols_tf=~cols.isin(Y_feature)
-    cols=cols[cols_tf]
-    X=data[cols].values
-    Y=data[Y_feature].values
-
-    return X,Y
+	return X,Y, cols
 
 def remove_outlier(X,Y,level_x,level_y):
     print(Y.shape,'Y shape before threshhold cut')
