5e164fff51b9d4c0a02f350d191785251773adb3
Henry Ashton <github@yetiminer.com>
2018-07-04 13:30:00 +0100

    added in general data formatting functions

--- secTools/general_loader.py
+++ secTools/general_loader.py
@@ -5,13 +5,28 @@ from sqlalchemy import create_engine, MetaData
 def ds_from_db(**kwargs):
 
 	cfg=kwargs['cfg']
-	db_name=kwargs['db_name']
+	threshold=kwargs['tag_min_count_threshold']
+	normalise_cols=kwargs['normalise_cols']
+	reimport_db=kwargs['reimport_db']
+	
+	
 	db_location=kwargs['db_location']
 	
 	engine=create_engine('sqlite:///'+db_location, echo=True)
 	data=load_from_db(engine)
 	data=streamline(data,cfg)
 	ds=SECdataset('ds',cfg,**data)
+	
+	ds.tag_count_plot(inplace=True)
+	ds.feature_prune(thresh=threshold,inplace=True)
+
+	ds.tag=ds.tag_prune(ds.tag)
+	ds.pre=ds.tag_prune(ds.pre)
+	ds.num=ds.tag_prune(ds.num)
+	
+	ds.create_feature_table(inplace=True)
+	ds.normalise(inplace=True,cols=normalise_cols)
+	
 	return ds
 	
 	
