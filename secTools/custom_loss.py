2dfedddb11b54e424dbd64cc245becc8ec33f8e1
Henry Ashton <github@yetiminer.com>
2018-07-15 09:43:21 +0100

    add function for correct denominator in loss

--- secTools/custom_loss.py
+++ secTools/custom_loss.py
@@ -13,7 +13,27 @@ def recon_loss_mse(y_true, y_pred):
     mask = K.cast(K.not_equal(y_true, mask_value), K.floatx())
 
     return mse(y_true*mask,y_pred*mask)
+	
+def other_recon_loss_mse(y_true, y_pred):
+	#this gets the numerator correct on the final denominator of the loss function
+	#does this make a difference? 
+	mask_value=0
+	mask = K.cast(K.not_equal(y_true, mask_value), K.floatx())
+	ones=K.ones(shape=K.shape(y_true))
+	n=K.sum(mask*ones,keepdims=False)
 
+	return 1/n*K.sum(K.square(y_true*mask-y_pred*mask))
+	
+def other_recon_loss_abs(y_true,y_pred):
+	#this gets the numerator correct on the final denominator of the loss function
+	#does this make a difference? 
+	mask_value=0
+
+	mask = K.cast(K.not_equal(y_true, mask_value), K.floatx())
+	ones=K.ones(shape=K.shape(y_true))
+	n=K.sum(mask*ones,keepdims=False)
+	return 1/n*K.sum(K.abs(y_true*mask-y_pred*mask))
+	
 
 def recon_loss_hinge(y_true,y_pred):
     mask_value=0
@@ -38,5 +58,13 @@ def make_recon_loss_combi(lamb):
         mask = K.cast(K.not_equal(y_true, mask_value), K.floatx())
 
         return lamb*mse(y_true*mask,y_pred*mask)+(1-lamb)*mean_absolute_error(y_true*mask,y_pred*mask)
+    
+    return recon_loss_combi
+	
+def make_other_recon_loss_combi(lamb):
+
+    def recon_loss_combi(y_true, y_pred):
+
+        return lamb*other_recon_loss_mse(y_true,y_pred)+(1-lamb)*other_recon_loss_abs(y_true,y_pred)
     
     return recon_loss_combi
