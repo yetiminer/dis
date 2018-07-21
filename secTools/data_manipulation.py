5e1037988f815a96ee20dea25a2a3010dc514d3d
Henry Ashton <github@yetiminer.com>
2018-07-21 20:30:58 +0100

    added in linear augmentation and data threshhold

--- secTools/data_manipulation.py
+++ secTools/data_manipulation.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import KernelDensity
+from functools import reduce
+from scipy.misc import comb
 
 def split_data_X_Y(df,cols):
 	#for the
@@ -58,22 +60,92 @@ def remove_outlier(X,level_x,Y=None,level_y=None,replace_nan=True,replace_with=0
 
 
 def augment_x(x_train,ds,repeats=2,fit_col='Assets',seed=22):
-    #augments data with random multiplactive constant on all present value columns
-    #distribution of this constant is that of the eg Assets column (which is assets growth)
-    
-    
-    aug_mask=list(map(lambda x: x[0]!='p',ds.FT.columns[0:200]))
-    
-    ker_fit_data=ds.FT[fit_col].values
-    ker_fit_data=ker_fit_data[(ker_fit_data>0.5)*(ker_fit_data<1.5)]
-    ker_fit_data=ker_fit_data.reshape(-1,1)
-    
-    kde = KernelDensity(kernel='gaussian', bandwidth=0.05).fit(ker_fit_data)
-    x_train_aug=np.repeat(x_train,repeats,axis=0)
-    
-    number=x_train_aug.shape[0]
-    seed=22
-    scale_rand=kde.sample([number], seed)
-    
-    x_train_aug[:,aug_mask]*scale_rand
-    return x_train_aug
+	#augments data with random multiplactive constant on all present value columns
+	#distribution of this constant is that of the eg Assets column (which is assets growth)
+
+
+	aug_mask=list(map(lambda x: x[0]!='p',ds.FT.columns[0:200]))
+
+	ker_fit_data=ds.FT[fit_col].values
+	ker_fit_data=ker_fit_data[(ker_fit_data>0.5)*(ker_fit_data<1.5)]
+	ker_fit_data=ker_fit_data.reshape(-1,1)
+
+	kde = KernelDensity(kernel='gaussian', bandwidth=0.05).fit(ker_fit_data)
+	x_train_aug=np.repeat(x_train,repeats,axis=0)
+
+	number=x_train_aug.shape[0]
+	seed=22
+	scale_rand=kde.sample([number], seed)
+
+	x_train_aug[:,aug_mask]*scale_rand
+	x_train[np.abs(x_train)>2.5]=0
+	return x_train_aug
+	
+
+def unique_pairs(arr):
+    uview = np.ascontiguousarray(arr).view(np.dtype((np.void, arr.dtype.itemsize * arr.shape[1])))
+    uvals, uidx = np.unique(uview, return_inverse=True)
+    pos = np.where(np.bincount(uidx) >= 2)[0]
+
+    pairs = []
+    for p in pos:
+        pairs.append(np.where(uidx==p)[0])
+
+    return np.array(pairs)
+	
+def new_datapoint(sample_pairs,x_train,pairs=False):    
+	#pick set at random
+	num_pairs=len(sample_pairs)
+
+	if pairs:
+	#so each possible pair has an equal chance of being picked
+		ridx = np.random.choice(range(1,len(sample_pairs)), 1, [comb(len(k),2) for k in sample_pairs])[0]
+	else:
+		ridx=np.random.randint(1, high=num_pairs)
+	match_set=sample_pairs[ridx]
+
+	if pairs:
+		num_match_set=2
+		match_set_pair=np.random.choice(match_set,2,replace=False)
+		weight_vec=sum_to_one_uniform(2)
+		out=np.matmul(weight_vec,x_train[match_set_pair])
+		out=np.reshape(out,(1,out.shape[0]))
+		
+	else:
+	#new sample is linear combo of all in matching set
+		num_match_set=len(match_set)
+		weight_vec=sum_to_one_uniform(num_match_set)
+		weight_vec=np.reshape(weight_vec,(1,weight_vec.shape[0]))
+
+		out=np.matmul(weight_vec,x_train[match_set])
+
+	out[np.abs(out)>2.5]=0
+	return out
+#add random linear combination of remaining financials 
+#normalise
+
+def sum_to_one_uniform(m):
+    k=np.random.uniform(0,1,m)
+    return k/sum(k)
+
+
+	
+def augment_x_linear(x_train,reps=20000,pairs=False):
+	#randomly combine pairs or tuples of financials with the same set of missing values
+	A=x_train!=0
+	sample_pairs=unique_pairs(A)
+	
+	def new_dp_wrap(x):
+		return new_datapoint(sample_pairs,x_train,pairs=pairs)
+	
+	num_financials=len(np.unique(reduce(lambda x,y: np.append(x,y),sample_pairs)))
+	
+	print("there are ",num_financials, " with at least one identical pair to compose new financials from")
+	
+	new_data=list(map(new_dp_wrap,range(0,reps)))
+	
+	new_data=np.vstack(new_data)
+	
+	return new_data
+	
+
